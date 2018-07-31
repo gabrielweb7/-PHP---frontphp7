9427a02c3b048c6385842e8178851a8c2b8c921d
Gabriel da Luz <gabrielbarbosaweb7@gmail.com>
2018-07-31 05:58:29 -0400

    Up!

--- class/includes/tools.class.php
+++ class/includes/tools.class.php
@@ -6,6 +6,24 @@
 	*/
 	class tools {
 
+		/* Função para retornar IP do visitante */
+		public static function getRealIp()
+		{
+			if ( !empty( $_SERVER['HTTP_CLIENT_IP'] ) )
+			{
+				$ip = $_SERVER['HTTP_CLIENT_IP'];
+			}
+			elseif( !empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) )
+			{
+				$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+			}
+			else
+			{
+				$ip = $_SERVER['REMOTE_ADDR'];
+			}
+			return $ip;
+		}
+
 		/** 
 		*	Verifica se URL remoto existe! 
 		*/
---
+++ demo-pages/tools-getRealIp.php
@@ -0,0 +1,9 @@
+<?php 
+
+	require_once("../init.php"); 
+
+	echo tools::getRealIp();
+
+	/* Retorno: */
+
+?>
