9e3de3693319950a4ece7468fa7ce47f0d2ae27a
Gabriel da Luz <gabrielbarbosaweb7@gmail.com>
2018-09-01 00:54:37 -0400

    Add area restrita class

---
+++ class/includes/areaRestrita.class.php
@@ -0,0 +1,71 @@
+<?php 
+	/**
+	*	Classe desenvolvida gerenciar area restrita do usuario
+	*	@author Gabriel Azuaga Barbosa
+	* 	@version 1.0.0
+	*/
+	class areaRestrita {
+
+		/**
+		*	Variavel que define nome da sessão de Status
+		*/
+		private static $nameSession = "statusLogged"; 
+
+		/**
+		*	Variavel que define valor positivo da sessão de Status
+		*/
+		private static $positiveStatus = "0x1"; 
+
+		/**
+		*	Função criada retornar status do login do usuario
+		*	Return: True ou False
+		*/
+		public static function getStatusLogged() {
+			/**
+			*	Se a sessão existe e ela contem o valor de 0x1
+			*	retorna true.. caso contrario retorna false
+			*/
+			if(isset($_SESSION[self::$nameSession]) and $_SESSION[self::$nameSession] = self::$positiveStatus) { 
+				return true;
+			}
+			return false;
+		}
+
+		/**
+		*	Função criada retornar status do login do usuario
+		*	Return: True ou False
+		*/
+		public static function setLogged($v = false) {
+			if($v) {
+				/**
+				*	Atribuir valor positivo para sessão de status de login
+				*/
+				$_SESSION[self::$nameSession] = self::$positiveStatus;
+			} else { 
+				/**
+				*	Destruir sessão
+				*/
+				unset($_SESSION[self::$nameSession]);
+			}
+		}
+
+		/**
+		*	Função criada para proteger pagina de acessos sem estar logado
+		*	Return: Em caso de false.. redirecionando para pagina de login!
+		*/
+		public static function isProtectedPage($urlRedirect) {
+		    /**
+	        *	Verifica Status de Loggin
+	        */
+	        if(!self::getStatusLogged()) {
+	            /**
+	            *   Redirecionar para pagina de login
+	            */
+	            jsTools::redirecionar($urlRedirect);
+	        } 
+		}
+
+
+	}
+
+?>
