dd0bb0fd5c08f6fa4451d44fff2f79f0be069195
Gabriel da Luz <gabrielbarbosaweb7@gmail.com>
2018-09-01 00:57:45 -0400

    Update Version 1.0.2

--- init.php
+++ init.php
@@ -9,7 +9,7 @@
 	*	Whats: +55 67 98466-0441
 	*
 	*	@author Gabriel Azuaga Barbosa
-	* 	@version 1.0.1
+	* 	@version 1.0.2
 	*	@criado 26/06/2018
 	*
 	*/
@@ -21,11 +21,22 @@
 	getConnect("pdo.class.php");
 	
 	/* Includes */
+	getInclude("areaRestrita.class.php");
 	getInclude("tools.class.php");
 	getInclude("upload.class.php");
 	getInclude("youtube.class.php");
 
 	/* Modulos Personalizados */
 	/* getModulo("nomedomodulo.php"); */
+	
+	/* Variaveis do Projeto */
+	define("WEBSITE_TITLE", "Website Titulo");
+    define("WEBSITE_DESCRICAO", "Website Descrição");
+    define("WEBSITE_AUTHOR", "Gabriel Azuaga Barbosa");
+
+
+
+	
+	
 	
 ?>
